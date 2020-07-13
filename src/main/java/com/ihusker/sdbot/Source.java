6a3013243311fa1a45f85d8b24489b48b24ac693
Brett O'Connor <brettoconnor@dogbheadbone.com>
2020-07-13 09:13:32 -0600

    added markdown escaper and some emoji for notifications

--- src/main/java/com/ihusker/sdbot/Source.java
+++ src/main/java/com/ihusker/sdbot/Source.java
@@ -44,53 +44,41 @@ public class Source extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 
-	@EventHandler
-	public void onQuit(PlayerQuitEvent event) {
-		Player player = event.getPlayer();
+	private String readyText(String str) {
+		String returnStr = str;
+		String[] markdownSpecialChars = new String[] {
+				//"\\"
+				"`",
+				"*",
+				"_",
+				"{",
+				"}",
+				"[",
+				"]",
+				"(",
+				")",
+				"#",
+				">",
+				"+",
+				"-",
+				".",
+				"!"
+		};
 
-		executorService.execute(() -> {
-			HttpURLConnection connection;
-			try {
-				connection = (HttpURLConnection) url.openConnection();
-
-				try {
-					connection.setRequestMethod("POST");
-				} catch (ProtocolException e) {
-					e.printStackTrace();
-				}
-
-				connection.setRequestProperty("Content-Type", "application/json; utf-8");
-				connection.setRequestProperty("Accept", "application/json");
-				connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11");
-				connection.setDoOutput(true);
-
-				Charset charset = StandardCharsets.UTF_8;
-				// trim off text formatting garbage
-				String eventString = event.getQuitMessage().substring(2);
-				try (OutputStream outputStream = connection.getOutputStream()) {
-					byte[] input = (
-							"{\"username\":\"" + player.getName() +
-									"\",\"content\":\"" + ":peace: _```" + eventString + "```_" +
-									"\",\"avatar_url\":\"https://crafatar.com/renders/head/" + player.getUniqueId() + "?overlay\"}"
-					).getBytes(charset);
-					outputStream.write(input, 0, input.length);
-				}
-
-				if (connection.getResponseCode() != HttpURLConnection.HTTP_CREATED) return;
-				try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), charset))) {
-					String inputLine;
-					StringBuilder response = new StringBuilder();
-					while ((inputLine = bufferedReader.readLine()) != null) {
-						response.append(inputLine);
-					}
-					getLogger().info(response.toString());
-				}
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		});
+		for (String character: markdownSpecialChars) {
+			returnStr.replace(character, "\\" + character);
+		}
+		return returnStr;
 	}
+
+	/*
+	* TODO
+	*
+	* - format messages escaping markdown with java
+	* - add advancement announcement https://jd.bukkit.org/org/bukkit/event/player/PlayerAchievementAwardedEvent.html
+	* - escaper doesnt work with backslash
+	*
+	* */
 
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event) {
@@ -113,12 +101,68 @@ public class Source extends JavaPlugin implements Listener {
 				connection.setDoOutput(true);
 
 				Charset charset = StandardCharsets.UTF_8;
-				// trim off text formatting garbage
-				String eventString = event.getJoinMessage().substring(2);
+
+				// we trim off the minecraft color formatting for this event
+				String originalMessage = event.getJoinMessage().substring(2);
+				// escapes any special markdown characters
+				String eventString = readyText(originalMessage);
+
 				try (OutputStream outputStream = connection.getOutputStream()) {
 					byte[] input = (
 							"{\"username\":\"" + player.getName() +
-									"\",\"content\":\"" + ":wave: _```" + eventString + "```_" +
+									"\",\"content\":\"" + ":green_circle: _" + eventString + "_" +
+									"\",\"avatar_url\":\"https://crafatar.com/renders/head/" + player.getUniqueId() + "?overlay\"}"
+					).getBytes(charset);
+					outputStream.write(input, 0, input.length);
+				}
+
+				if (connection.getResponseCode() != HttpURLConnection.HTTP_CREATED) return;
+				try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), charset))) {
+					String inputLine;
+					StringBuilder response = new StringBuilder();
+					while ((inputLine = bufferedReader.readLine()) != null) {
+						response.append(inputLine);
+					}
+					getLogger().info(response.toString());
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+
+		executorService.execute(() -> {
+			HttpURLConnection connection;
+			try {
+				connection = (HttpURLConnection) url.openConnection();
+
+				try {
+					connection.setRequestMethod("POST");
+				} catch (ProtocolException e) {
+					e.printStackTrace();
+				}
+
+				connection.setRequestProperty("Content-Type", "application/json; utf-8");
+				connection.setRequestProperty("Accept", "application/json");
+				connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11");
+				connection.setDoOutput(true);
+
+				Charset charset = StandardCharsets.UTF_8;
+
+				// we trim off the minecraft color formatting for this event
+				String originalMessage = event.getQuitMessage().substring(2);
+				// escapes any special markdown characters
+				String eventString = readyText(originalMessage);
+
+				try (OutputStream outputStream = connection.getOutputStream()) {
+					byte[] input = (
+							"{\"username\":\"" + player.getName() +
+									"\",\"content\":\"" + ":red_circle: _" + eventString + "_" +
 									"\",\"avatar_url\":\"https://crafatar.com/renders/head/" + player.getUniqueId() + "?overlay\"}"
 					).getBytes(charset);
 					outputStream.write(input, 0, input.length);
@@ -161,10 +205,15 @@ public class Source extends JavaPlugin implements Listener {
 				connection.setDoOutput(true);
 
 				Charset charset = StandardCharsets.UTF_8;
+
+				// escapes any special markdown characters
+				String originalMessage = event.getDeathMessage();
+				String eventString = readyText(originalMessage);
+
 				try (OutputStream outputStream = connection.getOutputStream()) {
 					byte[] input = (
 							"{\"username\":\"" + player.getName() +
-									"\",\"content\":\"" + ":skull_crossbones: _```" + event.getDeathMessage() + "```_" +
+									"\",\"content\":\"" + ":skull_crossbones: _" + eventString + "_" +
 									"\",\"avatar_url\":\"https://crafatar.com/renders/head/" + player.getUniqueId() + "?overlay\"}"
 					).getBytes(charset);
 					outputStream.write(input, 0, input.length);
@@ -207,10 +256,15 @@ public class Source extends JavaPlugin implements Listener {
 				connection.setDoOutput(true);
 
 				Charset charset = StandardCharsets.UTF_8;
+
+				// escapes any special markdown characters
+				String originalMessage = event.getMessage();
+				String eventString = readyText(originalMessage);
+
 				try (OutputStream outputStream = connection.getOutputStream()) {
 					byte[] input = (
 							"{\"username\":\"" + player.getName() +
-									"\",\"content\":\"" + ":speech_left: ```" + event.getMessage() + "```" +
+									"\",\"content\":\"" + eventString +
 									"\",\"avatar_url\":\"https://crafatar.com/renders/head/" + player.getUniqueId() + "?overlay\"}"
 					).getBytes(charset);
 					outputStream.write(input, 0, input.length);
